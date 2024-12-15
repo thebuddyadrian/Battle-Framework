8b44076553e3945fa9cab8e30dbe71fd7a103690
Rinniran <139382590+Rinniran@users.noreply.github.com>
2024-12-15 15:44:23 -0800

    important

--- camera.gd
+++ camera.gd
@@ -3,30 +3,30 @@ extends Camera3D
 class_name Camera
 
 
-@export var movement_speed := 5.0
-@export var rotation_speed := 5.0
+@export var movespeed := 5.0
+@export var rotspeed := 5.0
 
 @onready var root: Node3D = %CameraRoot
-@onready var position_target: Node3D = %CameraRoot/PositionTarget
+@onready var targetpos: Node3D = %CameraRoot/PositionTarget
 @onready var rotation_target: Node3D = %CameraRoot/RotationTarget
 
 
 func _process(delta):
-	var target_position := Vector3.ZERO
+	var targetposex := Vector3.ZERO
 
 	for character: BattleCharacter in get_tree().get_nodes_in_group("characters"):
-		target_position += character.global_position
+		targetposex += character.global_position
 	
-	target_position /= get_tree().get_node_count_in_group("characters")
+	targetposex /= get_tree().get_node_count_in_group("characters")
 
-	var target_position_2 := Vector3(target_position.x/1.3, target_position.y/1.2, target_position.z)
+	var targetpos2 := Vector3(targetposex.x/1.3, targetposex.y/1.2, targetposex.z)
 
-	position_target.global_position = position_target.global_position.lerp(
-		target_position_2, min(delta * movement_speed, 1.0))
+	targetpos.global_position = targetpos.global_position.lerp(
+		targetpos2, min(delta * movespeed, 1.0))
 	
 	rotation_target.global_position = rotation_target.global_position.lerp(
-		target_position, min(delta * rotation_speed, 1.0))
+		targetposex, min(delta * rotspeed, 1.0))
 	
-	root.global_position = position_target.global_position
+	root.global_position = targetpos.global_position
 	look_at_from_position(global_position, rotation_target.global_position)
 	return
