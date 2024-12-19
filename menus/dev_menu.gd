eb96c1351d68951df6a965c1439005093554e530
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2024-12-19 14:53:43 -0700

    Added scene switch menu to dev menu

--- menus/dev_menu.gd
+++ menus/dev_menu.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+@onready var scene_select_window:FileDialog = $FileDialog
+
+func finalize_switch_scene(path:String) -> void:
+	get_tree().change_scene_to_file(path)
+	scene_select_window.hide()
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	hide()
@@ -9,8 +15,9 @@ func _input(event: InputEvent) -> void:
 		visible = not visible #toggle visibility
 
 func _on_switch_scene_pressed() -> void:
-	#TODO: Menu here
-	pass # Replace with function body.
+	scene_select_window.show()
+	if not scene_select_window.is_connected(&"file_selected", finalize_switch_scene):
+		scene_select_window.connect(&"file_selected", finalize_switch_scene, CONNECT_ONE_SHOT)
 
 func _on_reboot_pressed() -> void:
 	get_tree().reload_current_scene()
