9dde05d9c22e91d55aa15250e4b497892ac8a520
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-03-21 14:12:54 -0700

    player setup goes straight to gameplay, no char select for now

--- menus/player_setup.gd
+++ menus/player_setup.gd
@@ -4,24 +4,33 @@ const MAX_PLAYERS = 4
 
 @onready var title_label: Label = $TitleLabel
 @onready var ok_label: Label = $OKLabel
+@onready var ok_button: Button = $OKButton
 @onready var human_spin_box: SpinBox = $GridContainer/HumanSpinBox
 @onready var cpu_spin_box: SpinBox = $GridContainer/CPUSpinBox
+@onready var stage_option_button: OptionButton = $GridContainer/StageOptionButton
 
 
 func _ready() -> void:
 	MusicPlayer.play_track(MusicPlayer.CHALLENGE_BATTLE_MODE)
+
+	# Initialize stage select
+	for stage in Lists.battle_stages:
+		stage_option_button.add_item(stage)
 
 
 func _process(delta: float) -> void:
 	var cpu_players: int = cpu_spin_box.value
 	var human_players: int = human_spin_box.value
 	var total_players: int = cpu_players + human_players
-	ok_label.visible = total_players >= 2
+	#ok_label.visible = total_players >= 2
+	ok_button.visible = total_players >= 2
 	if Input.is_action_just_pressed("ui_accept") and total_players >= 2:
-		SceneChanger.change_scene_to_file("res://menus/character_select/character_select.tscn")
+		#SceneChanger.change_scene_to_file("res://menus/character_select/character_select.tscn")
+		# SceneChanger.change_scene_to_file("res://levels/%s/%s.tscn" % [stage_option_button.text, stage_option_button.text])
 		
-		Game.cpu_players = cpu_players
-		Game.human_players = human_players
+		# Game.cpu_players = cpu_players
+		# Game.human_players = human_players
+		pass
 
 
 func _on_human_spin_box_value_changed(value: float) -> void:
@@ -30,3 +39,12 @@ func _on_human_spin_box_value_changed(value: float) -> void:
 
 func _on_cpu_spin_box_value_changed(value: float) -> void:
 	human_spin_box.max_value = MAX_PLAYERS - value
+
+
+func _on_ok_button_pressed() -> void:
+	SceneChanger.change_scene_to_file("res://levels/%s/%s.tscn" % [stage_option_button.text, stage_option_button.text])
+	var cpu_players: int = cpu_spin_box.value
+	var human_players: int = human_spin_box.value
+	Game.cpu_players = cpu_players
+	Game.human_players = human_players
+
