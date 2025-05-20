a425e42ee84c28aa6a0f43b73ce1683aefb090dc
Arthuralgum <arthur.arruda.mourao@gmail.com>
2025-05-20 18:15:49 -0300

    Fixed more of shadow's animations

--- characters/shadow/shadow.gd
+++ characters/shadow/shadow.gd
@@ -1,1 +1,13 @@
 extends BattleCharacter
+@onready var shadow_sprite = $PlayerSprite/EffectsSprites
+var flip_factor: int = 1
+func _process(delta: float) -> void:
+	shadow_sprite.flip_h = $PlayerSprite.flip_h
+	if shadow_sprite.flip_h:
+		flip_factor = -1
+	else:
+		flip_factor = 1
+	if animplayer.current_animation == "Upper":
+			shadow_sprite.position.x = 0.06 * flip_factor
+	elif animplayer.current_animation == "Heavy":
+		shadow_sprite.position.x = -0.02 * flip_factor
