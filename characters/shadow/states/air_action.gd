1ccd62acb342d1d90206bcb04dbf509e1358a7d7
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-05-21 18:47:42 -0700

    fix shadow air action

--- characters/shadow/states/air_action.gd
+++ characters/shadow/states/air_action.gd
@@ -9,23 +9,30 @@ func _enter(data = {}):
 	
 	root.animplayer.play("AirAction")
 	root.play_sound_effect("sonic_ballet")
-	# Sonic dashes in the facing their direction
-	root.velocity.y = 6
-	root.velocity.x = dash_direction.x * 15
-	root.velocity.z = dash_direction.y * 15
+	# Shadow dashes in the facing their direction
+	root.velocity.y = 0
+	root.velocity.x = dash_direction.x * 22
+	root.velocity.z = dash_direction.y * 22
+	root.deceleration_scale = 0
+	root.gravity_scale = 0
 	root.limit_speed = false
 	# Make sure to increment the amount of air actions used, to limit the amount of times they can do it until the touch the ground
 	root.air_actions_used += 1
 
 
 func _step():
-	if parent.state_time > 30:
+	if parent.state_time > 6:
+		root.velocity.x *= 0.1
+		root.velocity.z *= 0.1
+	if parent.state_time > 20:
 		parent.change_state("Fall")
 	if root.is_on_floor():
 		parent.change_state("Land")
 
 
 func _exit(next_state):
+	root.deceleration_scale = 1
 	root.limit_speed = true
+	root.gravity_scale = 1
 	root.animplayer.stop()
 	super._exit(next_state)
