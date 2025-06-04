33a1914ae36b147d53a55eb05c4082e6af8e3140
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-06-04 16:49:57 -0700

    added comment about knuckles skills being hardcoded

--- characters/knuckles/knuckles.gd
+++ characters/knuckles/knuckles.gd
@@ -1,6 +1,7 @@
 extends BattleCharacter
 
 
+#Right now Knuckles skills are hardcoded, this will be removed when the skill select menu is added
 func _check_for_ground_special():
 	if input("skill", "just_pressed") and !state_machine.active_state is BaseAttack:
 		state_machine.change_state("GrndPow")
