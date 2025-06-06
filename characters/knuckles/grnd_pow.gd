7035b936849eab24dc31e5e5b3b4c04b5b54a6ff
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-06-05 17:06:27 -0700

    added directional animations to knuckles GrndPow

--- characters/knuckles/grnd_pow.gd
+++ characters/knuckles/grnd_pow.gd
@@ -23,25 +23,26 @@ func _step():
 			explosion = true
 		root.velocity.x = attack_direction.x * 7
 		root.velocity.z = attack_direction.y * 7
-		if attack_direction == Vector2.RIGHT:
-			proj.position.x = root.position.x + proj_offset_x
-			proj.position.y = root.position.y + proj_offset_y
-			proj.position.z = root.position.z + proj_offset_z
-		elif attack_direction == Vector2.LEFT:
-			proj.position.x = root.position.x - proj_offset_x
-			proj.position.y = root.position.y + proj_offset_y
-			proj.position.z = root.position.z + proj_offset_z
-		elif attack_direction == Vector2.UP:
-			proj.position.y = root.position.y + proj_offset_y
-			proj.position.z = root.position.z - proj_offset_z
-			if $"../../PlayerSprite".flip_h:
+		if is_instance_valid(proj):
+			if attack_direction == Vector2.RIGHT:
+				proj.position.x = root.position.x + proj_offset_x
+				proj.position.y = root.position.y + proj_offset_y
+				proj.position.z = root.position.z + proj_offset_z
+			elif attack_direction == Vector2.LEFT:
+				proj.position.x = root.position.x - proj_offset_x
+				proj.position.y = root.position.y + proj_offset_y
+				proj.position.z = root.position.z + proj_offset_z
+			elif attack_direction == Vector2.UP:
+				proj.position.y = root.position.y + proj_offset_y
+				proj.position.z = root.position.z - proj_offset_z
+				if $"../../PlayerSprite".flip_h:
+					proj.position.x = root.position.x - 0.1
+				else:
+					proj.position.x = root.position.x + 0.15
+			elif attack_direction == Vector2.DOWN:
+				proj.position.y = root.position.y + 4*proj_offset_y
+				proj.position.z = root.position.z + 7*proj_offset_z
 				proj.position.x = root.position.x - 0.1
-			else:
-				proj.position.x = root.position.x + 0.15
-		elif attack_direction == Vector2.DOWN:
-			proj.position.y = root.position.y + 4*proj_offset_y
-			proj.position.z = root.position.z + 7*proj_offset_z
-			proj.position.x = root.position.x - 0.1
 	elif get_current_phase() == recovery_phase:
 		
 		root.velocity.x = lerp(attack_direction.x * 7, 0.0, 0.6)
