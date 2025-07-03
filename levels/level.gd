caddf9a85f72ee3f3cdf048b992542989183f569
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-07-03 16:53:20 -0700

    Added functionality to export stages as PCK files

--- levels/level.gd
+++ levels/level.gd
@@ -18,6 +18,8 @@ var camera_pivots: Dictionary[int, Node3D]
 var cameras: Dictionary[int, Camera3D]
 var audiostreamplayer = AudioStreamPlayer.new()
 
+@export_tool_button("Export as Mod PCK") var export_as_mod_pck = do_export_as_mod_pck
+
 
 func _ready() -> void:
 	if stage_info:
@@ -147,3 +149,49 @@ func _physics_process(delta: float) -> void:
 	for hitbox in hurt_areas:
 		for hurtbox in hurt_areas[hitbox]:
 			hitbox.do_hit(hurtbox)
+
+
+# --- MOD EXPORTING STUFF ---
+
+## Returns the name the stage will use internally to load files. 
+## For example, if the stage is saved in levels/greenhill/greenhill.tscn, the internal name will be greenhill
+func get_stage_internal_name():
+	var file_path_split = stage_info.resource_path.split("/")
+	var file_name = file_path_split[-1] # Last part of the path, the file name
+	return file_name.replace(".tres", "")
+
+
+func get_file_list(folder_path):
+	var file_paths: Array[String]
+	var dir =  DirAccess.open(folder_path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				#print("Found directory: " + folder_path + "/" + file_name)
+				file_paths.append_array(get_file_list(folder_path + "/" + file_name))
+			else:
+				#print("Found file: " + folder_path + "/" + file_name)
+				file_paths.append(folder_path + "/" + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	return file_paths
+
+
+func do_export_as_mod_pck():
+	var internal_name = get_stage_internal_name()
+	print("Exporting stage %s as mod..." % internal_name)
+	var folder_path = "res://levels/%s" % internal_name
+	# Create the folder to export mod if it doesn't exist
+	DirAccess.make_dir_recursive_absolute("res://mod_export")
+	# Create a PCKPacker instance and add all the stage files to the Mod PCK
+	var packer = PCKPacker.new()
+	packer.pck_start("res://mod_export/%s.pck" % internal_name)
+	for file_path in get_file_list(folder_path):
+		packer.add_file(file_path, file_path)
+	var err = packer.flush(true)
+	print(err)
+	#ResourceSaver.save(packer, "%s.pck" % internal_name)
+	
