82c11d12c84136fd32e58e16460967a0b25bb0d2
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-07-22 16:16:53 -0700

    moved character sprites into character folders

--- characters/knuckles/states/air_pow.gd
+++ characters/knuckles/states/air_pow.gd
@@ -1,5 +1,5 @@
 extends BaseAttack
-
+var has_jumped: bool = false
 func _enter(data := {}):
 	super._enter(data)
 	root.velocity.y = -24
@@ -10,12 +10,17 @@ func _enter(data := {}):
 
 func _step():
 	super._step()
-	if root.is_on_floor():
-		root.velocity.y = 14
-		root.velocity.x = attack_direction.x * 2
-		root.velocity.z = attack_direction.y * 2
+	$"../../PlayerSprite/Effects".show()
+	root.velocity.x = 0
+	root.velocity.z = 0
+	if root.is_on_floor() and get_current_phase() == active_phase and !has_jumped:
+		root.velocity.y = 0
+		has_jumped = true
+		await get_tree().create_timer(0.75).timeout
+		root.velocity.y = 25
+		has_jumped = false
 	
-	if parent.state_time % 4 == 0 and get_current_phase() == active_phase:
+	if parent.state_time % 10 == 0 and get_current_phase() == active_phase:
 		reactivate_hitbox()
 
 
