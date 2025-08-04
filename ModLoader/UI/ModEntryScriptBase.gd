65f768a4f829402ab05dba4b6982500fbde9463b
Stondonk <jastical66@gmail.com>
2025-08-04 15:26:02 +0100

    Temp mod menu added

---
+++ ModLoader/UI/ModEntryScriptBase.gd
@@ -0,0 +1,28 @@
+extends VBoxContainer
+
+@export var SubEntries:Array = []
+
+var SubModEntryObject = preload("res://ModLoader/UI/Mod_Menu_Single_SubEntry.tscn")
+var SubMenuOpen:bool = false
+
+func _ready() -> void:
+	OpenSubMenu(false)
+	pass
+	
+func UpdateSubEntries(_subs:Array) -> void:
+	for _child in $ModMenuArea/ScrollContainer/VBoxContainer.get_children():
+		_child.queue_free()
+	for _sub in _subs:
+		var SubEntry = SubModEntryObject.instantiate()
+		SubEntry.get_node("SubModButton/ItemName").text = _sub.get_file().get_basename()
+		$SubContainer.add_child(SubEntry)
+		
+		
+	SubEntries = _subs
+
+func OpenSubMenu(_o:bool) -> void:
+	SubMenuOpen = _o
+	$SubContainer.visible = SubMenuOpen
+
+func ToggleMenu() -> void:
+	OpenSubMenu(!SubMenuOpen)
---
+++ ModLoader/UI/ModMenuScriptBase.gd
@@ -0,0 +1,23 @@
+extends Control
+class_name Mod_Menu_Base
+
+@export var BaseModEntry = preload("res://ModLoader/UI/Mod_Menu_Single_ModEntry.tscn")
+var SubMenuOpen:bool = false
+
+func _ready() -> void:
+	UpdateModEntries()
+	
+	pass
+	
+func UpdateModEntries() -> void:
+	for _child in $ModMenuArea/ScrollContainer/VBoxContainer.get_children():
+		_child.queue_free()
+	for _mod in ModLoaderMaster.ModsByDir:
+		var CurrentModEntry = BaseModEntry.instantiate()
+		CurrentModEntry.get_node("ModTab/Label").text = _mod.get_file().get_basename()
+		$ModMenuArea/ScrollContainer/VBoxContainer.add_child(CurrentModEntry)
+		CurrentModEntry.UpdateSubEntries(ModLoaderMaster.ModsByDir[_mod])
+
+	
+func _process(delta: float) -> void:
+	pass
