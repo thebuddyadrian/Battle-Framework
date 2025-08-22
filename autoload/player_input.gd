72229fbfdb9a2e661e2020dfd67bbec7ef617b83
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-08-22 14:27:10 -0700

    add PlayerInput autoload class to get player-specific input

---
+++ autoload/player_input.gd
@@ -0,0 +1,15 @@
+# Helper class to get player-specific input for menus
+# In game, a function on base_player.gd will be used instead.
+extends Node
+
+
+func player_action_pressed(action: StringName, player_no: int):
+    return Input.is_action_pressed(action + str(player_no))
+
+
+func player_action_just_pressed(action: StringName, player_no: int):
+    return Input.is_action_just_pressed(action + str(player_no))
+
+
+func player_action_just_released(action: StringName, player_no: int):
+    return Input.is_action_just_released(action + str(player_no))
