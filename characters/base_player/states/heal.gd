d20d59acd4b5187bdbeb3367fc5e726b467c5be4
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-09-25 21:58:22 -0700

    add heal effect

--- characters/base_player/states/heal.gd
+++ characters/base_player/states/heal.gd
@@ -3,7 +3,7 @@ extends BaseState
 
 var startup: int = 8
 var recovery: int = 6
-
+var effect_instance: BaseEffect
 
 func _enter(data = {}):
 	pass
@@ -14,11 +14,21 @@ func _step():
 		root.animplayer.play_section_with_markers("Heal", "HealStartup", "HealLoop")
 	else:
 		root.animplayer.play_section_with_markers("Heal", "HealLoop", "HealEnd")
-		if parent.state_time % 12  == 0:
-			root.current_hp = min(root.current_hp + 1, root.max_hp)
+		if parent.state_time % 20  == 0:
+			root.current_hp = min(root.current_hp + 2, root.max_hp)
 			root.play_sound_effect("heal")
 
 		if !root.input("guard", "pressed"):
 			change_state("Idle")
 			return
 	
+	if parent.state_time == startup:
+		# Spawn Heal Effect
+		effect_instance = root.spawn_scene("HealEffect", "res://effects/heal_effect.tscn", root.global_position)
+	
+
+
+func _exit(next_state):
+	if effect_instance:
+		effect_instance.queue_free()
+		effect_instance = null
