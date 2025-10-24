2e5dc7e54d79935ad70b6b2f4deeba5d0258166c
thebuddyadrian <cosmicpegasus18@gmail.com>
2025-10-23 20:38:13 -0700

    add recovery animation to heal state

--- characters/base_player/states/heal.gd
+++ characters/base_player/states/heal.gd
@@ -4,14 +4,17 @@ extends BaseState
 var startup: int = 8
 var recovery: int = 6
 var effect_instance: BaseEffect
+var in_recovery: bool = false
 
 func _enter(data = {}):
-	pass
+	in_recovery = false
 
 
 func _step():
 	if parent.state_time <= startup:
 		root.animplayer.play_section_with_markers("Heal", "HealStartup", "HealLoop")
+	elif in_recovery:
+		root.animplayer.play_section_with_markers("Heal", "HealEnd", "")
 	else:
 		root.animplayer.play_section_with_markers("Heal", "HealLoop", "HealEnd")
 		if parent.state_time % 20  == 0:
@@ -19,13 +22,17 @@ func _step():
 			root.play_sound_effect("heal")
 
 		if !root.input("guard", "pressed"):
-			change_state("Idle")
+			in_recovery = true
+			parent.make_timer("go_to_idle", recovery, self)
 			return
 	
 	if parent.state_time == startup:
 		# Spawn Heal Effect
 		effect_instance = root.spawn_scene("HealEffect", "res://effects/heal_effect.tscn", root.global_position)
-	
+
+
+func go_to_idle():
+	change_state("Idle")
 
 
 func _exit(next_state):
