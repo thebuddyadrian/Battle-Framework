a6986b2f3f08b2b1898e027675965dde4a173cda
felipechoi <none@nowhere>
2026-03-10 02:26:31 -0300

    title screen scripting to transistion towards mode select menu

--- menus/TitleScreen/title_screen.gd
+++ menus/TitleScreen/title_screen.gd
@@ -3,11 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimationPlayer.play("Opening")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		$AnimationPlayer.play("Opening")
+	#if Input.is_action_just_pressed("ui_accept"):
+		#$AnimationPlayer.play("Opening")
+	pass
+	
+func animation_done_transition() -> void:
+	SceneChanger.change_scene_to_file("res://menus/mode_select.tscn")
 	pass
