8ecf957116f52b7dc1c4488650014f77fa3ae6d6
thebuddyadrian <cosmicpegasus18@gmail.com>
2026-03-18 20:23:03 -0700

    Knuckles GrndPow rework:
    - Added sound/voice
    - Instead of spawning a projectile with no hitbox, an effect is spawned
    - Retain velocity going into attack
    - Increase hitbox size and move it towards Knuckles' body to prevent
    move from missing when too close
    - Misc code cleanup

--- characters/knuckles/states/grnd_pow.gd
+++ characters/knuckles/states/grnd_pow.gd
@@ -1,54 +1,51 @@
 extends BaseAttack
-const GRND_POW_PROJ_PATH = "res://spawnables/Knuckles_GrndPow.tscn"
-var proj_offset_x: float = 1.5
-var proj_offset_y: float = 1.5
-var proj_offset_z: float = 1.5
-var explosion: bool = false
-var proj
+
+const GRND_POW_EFFECT_PATH = "uid://ivfy1nbpdsfh" # knuckles_grnd_pow_effect.tscn
+
+var effect_offset_x: float = 1.5
+var effect_offset_y: float = 1.5
+var effect_offset_z: float = 1.5
+
+
 func _enter(data := {}):
 	super._enter(data)
+	root.deceleration_enabled = false
+	root.velocity.x += attack_direction.x * 0.5
+	root.velocity.z += attack_direction.y * 0.5
+	root.play_voice_clip("knuckles/knuckles_grnd_pow")
 
 
-func _step():
-	super._step()
-	var data = {
-			"direction": attack_direction,
-		}
-	if get_current_phase() == startup_phase:
-		root.velocity.x = lerp(0.0, attack_direction.x * 7, 0.5)
-		root.velocity.z = lerp(0.0, attack_direction.y * 7, 0.5)
-	elif get_current_phase() == active_phase:
-		if !explosion:
-			proj = root.spawn_scene("GrndShot", GRND_POW_PROJ_PATH, root.global_position, null, data)
-			explosion = true
+func _phase_changed():
+	if get_current_phase() == active_phase:
+		root.play_sound_effect("explosion")
+		root.deceleration_enabled = true
+		root.velocity.x = attack_direction.x * 5
+		root.velocity.z = attack_direction.y * 5
+		root.deceleration_scale = 1
+		var data = {"direction": attack_direction}
+		var effect: BaseEffect = root.spawn_scene("GrndPowEffect", GRND_POW_EFFECT_PATH, 
+				root.global_position, null, data)
 		root.velocity.x = attack_direction.x * 7
 		root.velocity.z = attack_direction.y * 7
-		if is_instance_valid(proj):
-			if attack_direction == Vector2.RIGHT:
-				proj.position.x = root.position.x + proj_offset_x
-				proj.position.y = root.position.y
-				proj.position.z = root.position.z
-			elif attack_direction == Vector2.LEFT:
-				proj.position.x = root.position.x - proj_offset_x
-				proj.position.y = root.position.y
-				proj.position.z = root.position.z
-			elif attack_direction == Vector2.UP:
-				proj.position.x = root.position.x
-				proj.position.y = root.position.y 
-				proj.position.z = root.position.z - proj_offset_z
-			elif attack_direction == Vector2.DOWN:
-				proj.position.y = root.position.y 
-				proj.position.z = root.position.z + proj_offset_z
-				proj.position.x = root.position.x
-	elif get_current_phase() == recovery_phase:
-		
-		root.velocity.x = lerp(attack_direction.x * 7, 0.0, 0.6)
-		root.velocity.z = lerp(attack_direction.y * 7, 0.0, 0.6)
-	
-func _phase_changed():
-	if get_current_phase() == recovery_phase and proj:
-		explosion = false
-		proj.queue_free()
+		if attack_direction == Vector2.RIGHT:
+			effect.position.x = root.position.x + 1.5
+			effect.position.y = root.position.y
+			effect.position.z = root.position.z
+		elif attack_direction == Vector2.LEFT:
+			effect.position.x = root.position.x - 1.5
+			effect.position.y = root.position.y
+			effect.position.z = root.position.z
+		elif attack_direction == Vector2.UP:
+			effect.position.x = root.position.x
+			effect.position.y = root.position.y 
+			effect.position.z = root.position.z - 1.5
+		elif attack_direction == Vector2.DOWN:
+			effect.position.y = root.position.y 
+			effect.position.z = root.position.z + 1.5
+			effect.position.x = root.position.x
+
 
 func _exit(next_state):
 	super._exit(next_state)
+	root.deceleration_enabled = true
+	root.deceleration_scale = 1
