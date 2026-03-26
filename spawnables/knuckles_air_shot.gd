3e4370611982e956848bef57533a7731e95e67ff
thebuddyadrian <cosmicpegasus18@gmail.com>
2026-03-25 20:30:20 -0700

    knuckles air shot projectile now hits twice
    It now has a direct hitbox (25 dmg) and an indirect hitbox (15 dmg)

--- spawnables/knuckles_air_shot.gd
+++ spawnables/knuckles_air_shot.gd
@@ -1,6 +1,8 @@
 extends BaseSpawnable
 
 @onready var rock_break_sfx: AudioStreamPlayer3D = $RockBreakSFX
+@onready var direct_hit_shape: CollisionShape3D = $Hitbox/DirectHitShape
+@onready var explosion_shape: CollisionShape3D = $Hitbox/ExplosionShape
 
 func _on_spawn(data: = {}):
 	sprite.show()
@@ -10,12 +12,21 @@ func _on_spawn(data: = {}):
 		sprite.flip_h = true
 	elif direction == Vector2.RIGHT:
 		sprite.flip_h = false
+	# Direct hit hitbox does 25 damage
+	hitbox.hit_data.damage = 25
 
 
 func _do_behavior(delta):
 	if is_on_floor() and !sprite.animation == "Break":
 		velocity = Vector3.ZERO
+		# Enable explosion shape so the hitbox becomes bigger after breaking
+		direct_hit_shape.disabled = true
+		explosion_shape.disabled = false
 		sprite.play("Break")
+		# Reset hitbox so it can hit again
+		hitbox.active = true
+		# Explosion hitbox does 15 damage
+		hitbox.hit_data.damage = 15
 		if !rock_break_sfx.playing:
 			rock_break_sfx.play()
 
