5f80cf654508f6fc92fdb87860d0664f0e763d58
Stondonk <jastical66@gmail.com>
2026-05-21 14:39:12 +0100

    Better following
    Actually added algorithm for moving toward point for AI nav

--- components/AI_devices/AI_Navigation_Behaviour_Base.gd
+++ components/AI_devices/AI_Navigation_Behaviour_Base.gd
@@ -5,8 +5,10 @@ class_name AI_Navigation_Behaviour_Base
 @export var character:BattleCharacter = null
 @export var input_device:PL_Input_Device = null
 
-var current_target:BattleCharacter = null
+@export var min_distance_to_target:float = 2.0
 
+var current_target:Node3D = null
+var current_target_char:BattleCharacter = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,19 +24,41 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(current_target != null): # Temp behaviour
-		var H_Dir = "left"
-		var V_Dir = "up"
 		
-		if(current_target.global_position.x > character.global_position.x):
-			H_Dir = "right"
-		if(current_target.global_position.z > character.global_position.z):
-			V_Dir = "down"
-			
-		input_device.Call_Input(H_Dir)
-		input_device.Call_Input(V_Dir)
+		var Target_Diff:Vector3 = current_target.global_position-character.global_position
+		var Target_Distance:float = Target_Diff.length()
+		#normlize without extra calulations
+		if(Target_Distance>0): Target_Diff/=Target_Distance
+		
+		if(Target_Distance > min_distance_to_target):
+			move(-Target_Diff)
 		
 	
 	pass
+	
+func move(Direction) -> void:
+		
+	const AngleRefOverlap:float = 60
+	
+	var Diff_Angle:float = (atan2(Direction.x,Direction.z))
+	
+	#Temp reconfig later
+	var H_Dir = ""
+	var V_Dir = ""
+	
+	if(abs(rad_to_deg(angle_difference(Diff_Angle,0))) < (AngleRefOverlap)):
+		V_Dir = "up"
+	elif(abs(rad_to_deg(angle_difference(Diff_Angle,PI))) < (AngleRefOverlap)):
+		V_Dir = "down"
+	if(abs(rad_to_deg(angle_difference(Diff_Angle,PI/2))) < (AngleRefOverlap)):
+		H_Dir = "left"
+	elif(abs(rad_to_deg(angle_difference(Diff_Angle,-PI/2))) < (AngleRefOverlap)):
+		H_Dir = "right"
+	
+	if(H_Dir != ""): input_device.Call_Input(H_Dir)
+	if(V_Dir != ""): input_device.Call_Input(V_Dir)
+	
+	print("AT ",Diff_Angle, " ",H_Dir, " ",V_Dir)
 	
 func find_nearest_target() -> BattleCharacter:
 	var Chars = get_tree().get_nodes_in_group("characters")
@@ -56,6 +80,7 @@ func find_nearest_target() -> BattleCharacter:
 			ClosestDistIDX = CharacterIDX
 			ClosestDistRef = Dist
 	
+	current_target_char = Chars[ClosestDistIDX]
 	current_target = Chars[ClosestDistIDX]
 	return current_target
 	
