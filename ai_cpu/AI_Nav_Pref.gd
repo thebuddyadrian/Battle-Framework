13361422c7e6bfd5f3b7c0fe99a39d3827546e0d
Stondonk <jastical66@gmail.com>
2026-07-06 22:23:53 +0100

    better more agreeive behaviour options added
    Key word, OPTIONS added the aggression system isnt in yet, but it boils down to the more a character is hit the more aggression, ticking down to 0 which is the base. Then based on the level of aggression they could reduce time between attacks, cooldown and other things, but is unfinished at the moment

---
+++ ai_cpu/AI_Nav_Pref.gd
@@ -0,0 +1,13 @@
+extends Resource
+## Contains characater behaviour preferences such as if it can shoot a projectile in different states, ext..
+class_name AI_NAV_Pref
+
+@export var Jab1_Projectile:bool = false
+@export var Jab2_Projectile:bool = false
+@export var Jab3_Projectile:bool = false
+
+@export var Heavy_Projectile:bool = false
+
+@export var Upper_Projectile:bool = false
+
+@export var Aggresion_level_primary:Dictionary = {}
--- components/AI_devices/AI_Navigation_Behaviour_Base.gd
+++ components/AI_devices/AI_Navigation_Behaviour_Base.gd
@@ -1,6 +1,15 @@
 extends Node
 class_name AI_Navigation_Behaviour_Base
 
+@export var AI_Behaviour_settings:AI_NAV_Pref = preload("res://ai_cpu/DefaultAI.tres")
+# Currently non functional, place holder until attacking functions are added
+@export_range(1,5) var AI_Level:int = 1
+# Every hit gets them angrier which causes the time between attacks to decrease
+var Current_Agression_range:float = 0.0
+
+# Contains items which cannot be target by AI
+@export var blacklisted_targets:Array[Node] = []
+
 # Main body this component is attached too
 @export var character:BattleCharacter = null
 @export var input_device:PL_Input_Device = null
@@ -19,6 +28,8 @@ var current_path_points:PackedVector3Array = []
 var current_path_idx:int = 0
 
 var current_path_time:float = 0.0
+# time spent on moving to next point
+var current_point_time:float = 0.0
 
 var jump_cooldown:float = 0.0
 
@@ -39,8 +50,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	if(jump_cooldown > 0): jump_cooldown -= delta
+	
+	if(Current_Agression_range > 0): Current_Agression_range -= delta
 		
 	if(current_target != null): # Temp behaviour
+		
+		if(path_reset):
+			reset_path()
 		
 		var Grounded:bool = character.is_on_floor()
 		
@@ -49,12 +65,12 @@ func _process(delta: float) -> void:
 			current_path_time += delta
 			
 			# Generate path when cooldown is complete
-			if(current_path_time >= .5 || (path_reset && Grounded)): 
-				current_path_idx = 0
-				calculate_path_to(current_target.global_position)
-				current_path_time = 0.0
-				path_reset = false
+			if(current_path_time >= .5): 
+				path_reset = true
 		else:
+			
+			current_point_time += delta
+			
 			var point_dif = (current_path_points[current_path_idx+1]-current_path_points[current_path_idx])
 			var next_height = point_dif.dot(Vector3.UP)
 			var current_height = (current_path_points[current_path_idx]-character.global_position).dot(Vector3.UP)
@@ -65,7 +81,13 @@ func _process(delta: float) -> void:
 			if((next_height >= 0.1 || current_height >= 0.05) && jump_cooldown <= 0.0):
 				jump()
 		
-			if(point_dis <= 0 && Grounded): move_to_next_point()
+			if(point_dis <= 0 && Grounded): 
+				move_to_next_point()
+				current_point_time = 0.0
+				
+			# if the characters been targeting one spot for too long, reset
+			if(current_point_time > 1.0):
+				path_reset = true
 			
 			input_device.OverrideLeftStick = false
 			if(current_path_idx < current_path_points.size() || point_dis > min_distance_to_target): 
@@ -75,6 +97,15 @@ func _process(delta: float) -> void:
 				#move(point_dif.normalized())
 	
 	pass
+	
+	
+func reset_path() -> void:
+	# Generate path when cooldown is complete
+	current_path_idx = 0
+	calculate_path_to(current_target.global_position)
+	current_path_time = 0.0
+	current_point_time = 0.0
+	path_reset = false
 	
 func move_to_next_point() -> void:
 	current_path_idx+=1
@@ -91,34 +122,20 @@ func calculate_path_to(Target:Vector3) -> PackedVector3Array:
 	
 	return current_path_points
 	
-func Damaged() -> void:
+func Damaged(hitdata:HitData, hitbox:Hitbox) -> void:
+	# will need to be change in new update to find parent from hitbox
+	var HitFrom = hitbox.get_parent_node_3d()
+	if(blacklisted_targets.count(HitFrom) <= 0):
+		current_target = hitbox.get_parent_node_3d()
+		current_target_char = current_target as BattleCharacter
+	
+	Current_Agression_range = clamp(1.0,0,AI_Level * 2)
 	path_reset = true
 	
 func move(Direction) -> void:
-	
+	# Sets the stick override then, sets the input device to use the new left stick override
 	input_device.OverrideLeftStick = true
 	input_device.LocalOverrideLeftStick = Vector2(Direction.x,Direction.z)
-	#
-	#const AngleRefOverlap:float = 80
-	#
-	#var Diff_Angle:float = (atan2(Direction.x,Direction.z))
-	#
-	##Temp reconfig later
-	#var H_Dir = ""
-	#var V_Dir = ""
-	#
-	#if(abs(rad_to_deg(angle_difference(Diff_Angle,0))) < (AngleRefOverlap)):
-		#V_Dir = "up"
-	#elif(abs(rad_to_deg(angle_difference(Diff_Angle,PI))) < (AngleRefOverlap)):
-		#V_Dir = "down"
-	#if(abs(rad_to_deg(angle_difference(Diff_Angle,PI/2))) < (AngleRefOverlap)):
-		#H_Dir = "left"
-	#elif(abs(rad_to_deg(angle_difference(Diff_Angle,-PI/2))) < (AngleRefOverlap)):
-		#H_Dir = "right"
-	#
-	##Move base on require inputs for direction
-	#if(H_Dir != ""): input_device.Call_Input(H_Dir)
-	#if(V_Dir != ""): input_device.Call_Input(V_Dir)
 
 func jump() -> void:
 	input_device.Call_Input("jump")
@@ -126,7 +143,7 @@ func jump() -> void:
 	
 func find_nearest_target() -> BattleCharacter:
 	var Chars = get_tree().get_nodes_in_group("characters")
-	Chars.remove_at(Chars.find(character))
+	Chars.erase(character)
 	
 	if Chars.size() <= 0 : return null
 	if Chars.size() <= 1 : 
@@ -140,7 +157,7 @@ func find_nearest_target() -> BattleCharacter:
 	for CharacterIDX in range(1,Chars.size()):
 		var Diff = Vector3(Chars[CharacterIDX].global_position - character.global_position)
 		var Dist = Diff.length()
-		if(Dist <= ClosestDistRef):
+		if(Dist <= ClosestDistRef ):
 			ClosestDistIDX = CharacterIDX
 			ClosestDistRef = Dist
 	
