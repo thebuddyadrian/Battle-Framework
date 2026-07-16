897e1ed9022e97dc547d1d8863ece893561373b5
thebuddyadrian <cosmicpegasus18@gmail.com>
2026-07-15 21:02:25 -0700

    MapSelect: use UI actions

--- menus/character_select/mapselect.gd
+++ menus/character_select/mapselect.gd
@@ -13,19 +13,19 @@ func _ready() -> void:
 	stages = GameData.battle_stages
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("right1"):
+	if Input.is_action_just_pressed("ui_right"):
 		stage_step += 1
 		if stage_step >= stages.size():
 			stage_step = 0
 		Update_UI_Visuals(stages[stage_step], null)
 		
-	if Input.is_action_just_pressed("left1"):
+	if Input.is_action_just_pressed("ui_left"):
 		stage_step -= 1
 		if stage_step <= 0:
 			stage_step = stages.size() - 1
 		Update_UI_Visuals(stages[stage_step], null)
 		
-	if Input.is_action_just_pressed("attack1"):
+	if Input.is_action_just_pressed("ui_accept"):
 		if selected_stages.size() < max_selected_stages:
 			selected_stages.append(stages[stage_step])
 			var new_nametag = Label.new()
