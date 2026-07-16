8a54d0b8b3ca369a11a1fd6e7c00c3be5e838aa2
thebuddyadrian <cosmicpegasus18@gmail.com>
2026-07-15 21:34:32 -0700

    Shadow: make energy sounds match animation better

--- characters/shadow/states/heavy.gd
+++ characters/shadow/states/heavy.gd
@@ -1,14 +1,11 @@
 extends BaseAttack
 
-
-func _phase_changed():
-	if get_current_phase_name() == "active":
-		root.play_voice_clip("shadow/shadow_heavy")
-
 func _step():
 	if get_current_phase() == active_phase:
 		root.velocity.y = 1
 		root.gravity_scale = 0.5
+	if parent.state_time == 10:
+		root.play_voice_clip("shadow/shadow_heavy")
 	super._step()
 
 
--- characters/shadow/states/upper.gd
+++ characters/shadow/states/upper.gd
@@ -6,3 +6,6 @@ func _step():
 	# Experimental: Allow jump cancels on hit
 	if move_hit:
 		root.set_action_enabled("jump", true)
+	if parent.state_time == 1:
+		root.play_voice_clip("shadow/shadow_heavy")
+	
